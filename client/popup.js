72957810a92f7217034082d7a6ff5dd076e4801a
Alex Zhang <alexczhang08@gmail.com>
2024-01-16 15:26:49 -0500

    prep for aws

--- client/popup.js
+++ client/popup.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //Event listeners for each desired action
     const searchBtn = document.getElementById('searchBtn');
     const amazonBtn = document.getElementById('amazonBtn');
+    const walmartBtn = document.getElementById('walmartBtn');
     const otherBtn = document.getElementById('otherBtn');
     //Load data from api calls
     searchBtn.addEventListener('click', async function() {
@@ -21,6 +22,15 @@ document.addEventListener('DOMContentLoaded', function() {
         displayAmazon(data.deals.amazon);
       } else {
         console.log('No Amazon data available');
+      }
+    });
+    //Display Walmart data
+    walmartBtn.addEventListener('click', function() {
+      console.log(data);
+      if (data && data.deals.walmart) {
+        displayAmazon(data.deals.walmart);
+      } else {
+        console.log('No Walmart data available');
       }
     });
     //Display other data (currently nothing)
@@ -73,4 +83,25 @@ document.addEventListener('DOMContentLoaded', function() {
   
     // Display the deals list in the resultDiv
     resultDiv.innerHTML = `<p>Found ${source.length} deals:</p><ul>${dealsList}</ul>`;
+  }
+
+  function displayWalmart(source) {
+    const resultDiv = document.getElementById('result');
+
+    // Create a list of deals with price and image information
+    const dealsList = source.map(deal => {
+      const title = deal.title;
+      const price = deal.price ? `${deal.price.symbol}${deal.price.amount}` : 'Price not available';
+      const image = deal.images.length > 0 ? `<a href="https://www.amazon.com/dp/${deal.asin}"><img src="${deal.images[0].image}" alt="Product Image" /></a>` : 'Image not available';
+  
+      // Display title, price, and image for each deal
+      return `<li>
+                <a href="https://www.amazon.com/dp/${deal.asin}"><p><strong>${title}</strong></p></a>
+                <p>${price}</p>
+                <p>${image}</p>
+              </li>`;
+    }).join('');
+  
+    // Display the deals list in the resultDiv
+    resultDiv.innerHTML = `<p>Found ${source.length} deals:</p><ul>${dealsList}</ul>`;    
   }
